04840e25f1ad23d04ff0b32aa28460e6fb39a4ab
dan5 <dan5.ya@gmail.com>
2012-03-01 08:05:21 +0900

    rails new dollmaster -m http://railswizard.org/6167b62b47bdb1525f37.rb -J -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dollmaster::Application.config.secret_token = '63817ec3ad002ea3d738226424166f32bc60c9b4cd6909ecc0f37718883d52882dab4eab03a97d09643455a3dfd23c0ee59f222cd258230d80fd83a65fa68450'
